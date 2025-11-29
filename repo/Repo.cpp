25568e0880bd0d0c15c70af1a94702b025fb7e33
Saif Ahmed <seefahmed259@gmail.com>
2025-11-29 23:48:28 +0200

    implement StudentRepo addStudent

--- repo/Repo.cpp
+++ repo/Repo.cpp
@@ -1,11 +1,10 @@
+#include "../model/Models.cpp"
 #include <bits/stdc++.h>
 using namespace std;
 
 ////////////////// Teacher \\\\\\\\\\\\\\\\\\
 // interface TeacherRepository
 class TeacherRepository {
-
-
 
 
 
@@ -63,26 +62,27 @@ class CourseRepositoryImpl : public CourseRepository {
 class StudentRepository {
 
 
+  public :
 
-
-
-
-
-
-
-
+      virtual void addStudent(Student &student) = 0;
 };
 
 // class StudentRepository implementation
-class StudentRepositoryImpl : StudentRepository {
+class StudentRepositoryImpl : public StudentRepository {
 
+  int maxStudentsPerGrade = 320 ;
+  vector <Student> students;
 
+  public :
 
-
-
-
-
-
-
+    void addStudent (Student &student){
+       if (students.size() >= maxStudentsPerGrade){
+         cout <<"Registration full. Cannot add more students." << endl;
+       }
+       else {
+         students.push_back(student);
+          cout << "Added successfully." << endl;
+       }
+     }
 
 };
