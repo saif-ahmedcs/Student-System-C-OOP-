1e7e87cf39e1036eaf7bb5b78811b9c89ccb1fce
Saif Ahmed <seefahmed259@gmail.com>
2025-11-29 22:46:10 +0200

    Repo(Teacher - Course - Student)

--- model/Repo.cpp
+++ model/Repo.cpp
@@ -1,1 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+////////////////// Teacher \\\\\\\\\\\\\\\\\\
+// interface TeacherRepository
+class TeacherRepository {
+
+
+
+
+
+
+
+
+
+
+};
+// class TeacherRepository implementation
+class TeacherRepositoryImpl : public TeacherRepository {
+
+
+
+
+
+
+
+
+
+
+};
+
+////////////////// Course \\\\\\\\\\\\\\\\\\
+// interface CourseRepository
+class CourseRepository {
+
+
+
+
+
+
+
+
+
+
+};
+
+// class CourseRepository implementation
+class CourseRepositoryImpl : public CourseRepository {
+
+
+
+
+
+
+
+
+
+
+};
+
+////////////////// Student \\\\\\\\\\\\\\\\\\
+// interface StudentRepository
+class StudentRepository {
+
+
+
+
+
+
+
+
+
+
+};
+
+// class StudentRepository implementation
+class StudentRepositoryImpl : StudentRepository {
+
+
+
+
+
+
+
+
+
+
+};
