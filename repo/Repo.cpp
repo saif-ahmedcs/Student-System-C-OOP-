66516de34ea3d4521a71f3ddb1757ebd67922240
Saif Ahmed <seefahmed259@gmail.com>
2025-12-02 04:41:26 +0200

    Improve addStudent using map and clean code
    - Use map<int, vector<Student>> to store students by grade
    - Add constants for max students and grade limits
    - Ensure method returns bool for success/failure
    - Rename variables to follow clean code rules

--- repo/Repo.cpp
+++ repo/Repo.cpp
@@ -32,20 +32,29 @@ class TeacherRepositoryImpl : public TeacherRepository {
 // interface CourseRepository
 class CourseRepository {
 
+  public :
 
-
-
-
-
-
-
-
+      virtual void addCourse(Course &course) = 0;
 
 };
 
 // class CourseRepository implementation
 class CourseRepositoryImpl : public CourseRepository {
 
+  int MaxNumberOfCoursesPerGrade = 10;
+  vector <Course> courses;
+
+  public :
+
+    virtual void addCourse(Course &course){
+       if (students.size() >= maxStudentsPerGrade){
+         cout <<"Registration full. Cannot add more students." << endl;
+       }
+       else {
+         students.push_back(student);
+          cout << "Added successfully." << endl;
+       }
+     }
 
 
 
@@ -64,24 +73,28 @@ class StudentRepository {
 
   public :
 
-      virtual void addStudent(Student &student) = 0;
+      virtual bool addStudent(int grade, Student &student) = 0;
 };
 
 // class StudentRepository implementation
 class StudentRepositoryImpl : public StudentRepository {
 
-  int maxStudentsPerGrade = 320 ;
-  vector <Student> students;
+  const int maxStudentsPerGrade = 120 ;
+  const int firstGrade = 1;
+  const int lastGrade = 12;
+  map <int,vector<Student>> studentsByGrade;
 
   public :
 
-    void addStudent (Student &student){
-       if (students.size() >= maxStudentsPerGrade){
-         cout <<"Registration full. Cannot add more students." << endl;
+    bool addStudent(int grade, Student &student) override {
+
+       if (grade < firstGrade || grade > lastGrade || studentsByGrade[grade].size() >= maxStudentsPerGrade ){
+        return false;
        }
+
        else {
-         students.push_back(student);
-          cout << "Added successfully." << endl;
+         studentsByGrade[grade].push_back(student);
+          return true;
        }
      }
 
