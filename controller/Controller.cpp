c1f0ef823448884dd31e78b2a992ae95fcda4904
Saif Ahmed <seefahmed259@gmail.com>
2025-12-04 11:59:02 +0200

    Setup initial Controller Layer structure for Teacher, Course, and Student
    - Created Controller classes for handling user requests:
        • TeacherController
        • CourseController
        • StudentController
    - Each Controller holds a reference to its corresponding Service.
    - Implemented basic method signatures:
        • addTeacher
        • addCourse
        • addStudent
    - Controllers currently delegate calls directly to Services.
    - Layer is ready for connecting with UI in future commits.

--- controller/Controller.cpp
+++ controller/Controller.cpp
@@ -1,1 +1,40 @@
+#include "../service/Service.cpp"
+#include <bits/stdc++.h>
+using namespace std;
 
+class TeacherController {
+
+private :
+    TeacherService &teacherService;
+
+public :
+    bool addTeacher(int grade, Teacher &teacher){
+      return teacherService.addTeacher(grade,teacher);
+    }
+};
+
+
+class CourseController {
+
+private :
+    CourseService &courseService;
+
+public :
+    bool addCourse(int grade, Course &course){
+      return courseService.addCourse(grade,course);
+    }
+};
+
+
+class StudentController {
+
+private :
+    StudentService &studentService;
+
+public :
+
+    bool addStudent(int grade, Student &student){
+      return studentService.addStudent(grade,student);
+     }
+
+};
