4963be715a5602e7722a9659f1153af67a8e529b
Saif Ahmed <seefahmed259@gmail.com>
2025-12-04 16:09:38 +0200

    displaySystem() - showList()

--- start.cpp
+++ start.cpp
@@ -1,34 +1,43 @@
 #include <iostream>
 using namespace std;
 
+
+void displaySystem(){
+
+cout <<"\t********************* Welcome To Student System *********************"<<endl<<endl;
+   cout << endl;
+   cout <<"Please enter the process you need ! "<<endl;
+   cout <<"1- About Student" <<"\t" <<"2- About Course"<<endl;
+   cout <<"3- About Teacher" <<"\t" <<"4- Exit" << endl;
+}
+
+void showProcesses(string s){
+
+  cout <<"1- Add "<< s <<"\t\t" <<"2- Remove "<< s <<endl;
+  cout <<  "3- Edit "<< s << "\t\t" << "4- Show " << s <<" Info" <<endl;
+}
+
+
 int main()
 {
 
-cout <<"\t********************* Welcome To Student System *********************"<<endl<<endl;
 
-
+displaySystem();
 
 int process;
 bool flag = true;
 
  while (flag){
-   cout << endl;
-   cout <<"Enter Process : "<<endl<<endl;
-   cout <<"1- Add Student"<<"\t\t"<<"2- Remove Student"<<endl<<endl;
-   cout <<"3- Edit Student"<<"\t\t"<<"4- Show Student"<<endl<<endl;
-   cout <<"5- Exit"<<endl;
       cin >> process;
 
     switch (process){
-        case 1: cout <<"Add Student"<<endl;
+        case 1: showProcesses("Student");
             break;
-        case 2: cout <<"Remove Student"<<endl;
+        case 2: showProcesses("Course");
             break;
-        case 3: cout <<"Edit Student"<<endl;
+        case 3: showProcesses("Teacher");
             break;
-        case 4: cout <<"Show Student"<<endl;
-            break;
-        case 5: cout <<"Exit"<<endl;
+        case 4: cout <<"Exit"<<endl;
           flag = false;
             break;
         default: cout << "Invalid"<<endl;
@@ -37,8 +46,6 @@ bool flag = true;
 
 
  }
-
-
 
 
     return 0;
